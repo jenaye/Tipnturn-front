4cb0e7083420e14003f0275cfb0e20a4e61a250f
Houziaux mike <stahlilistenvoid@gmail.com>
2017-11-09 23:45:30 +0100

    start adding value, i got all value in console

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -9,22 +9,27 @@ import { ActivitiesService } from '../../services/activities.service';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-    public barChartOptions:any = {
+
+    constructor(private listemembre: ListingmembreService,
+                private activiteService: ActivitiesService) { }
+
+    public membres = [];
+    public activities = [];
+    public barChartOptions: any = {
         scaleShowVerticalLines: false,
         responsive: true,
         scales: {
             yAxes: [{id: 'y-axis-1', type: 'linear', position: 'left', ticks: {min: 0, max:20}}]
         }
     };
-    public barChartLabels:string[] = ['Activites'];
-    public barChartType:string = 'bar';
-    public barChartLegend:boolean = true;
-
-    public barChartData:any[] = [
+    public barChartLabels: string[] = ['Activites'];
+    public barChartType: string = 'bar';
+    public barChartLegend: boolean = true;
+    public barChartData: any[] = [
         {data: [11], label: 'Cours 1'},
-        {data: [9], label: 'Atelier 1'},
-        {data: [11], label: 'Cours 2'},
-        {data: [11], label: 'Atelier 2'}
+        {data: [8], label: 'Atelier 1'},
+        {data: [10], label: 'Cours 2'},
+        {data: [12], label: 'Atelier 2'}
     ];
 
     public chartColors: any[] = [
@@ -32,16 +37,10 @@ export class HomepageComponent implements OnInit {
             backgroundColor:["#5cb85c", "red", "#FAFFF2", "#FFFCC4", "#B9E8E0"]
         }];
 
-
-    // Doughnut
     public doughnutChartLabels:string[] = ['Rentrée', 'Sortie'];
     public doughnutChartData:number[] = [1003, 13];
     public doughnutChartType:string = 'doughnut';
 
-    // events
-    constructor(private listemembre: ListingmembreService,  private activiteService: ActivitiesService) { }
-    public membres = [];
-    public activities = [];
     ngOnInit() {
         this.listemembre.getData().subscribe(membres => {
             this.membres = membres;
@@ -49,7 +48,10 @@ export class HomepageComponent implements OnInit {
 
         this.activiteService.getData().subscribe(activities => {
             this.activities = activities;
-        })
+            activities.filter(function (el) {
+                console.log(el.membres.length);
+            });
+        });
     }
 
 }
