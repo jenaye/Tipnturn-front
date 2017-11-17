aaa84577adc26dbe01a9939b68aa1653f00b3809
Houziaux mike <stahlilistenvoid@gmail.com>
2017-11-17 10:10:54 +0100

    mb last fix ? hope work now

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,3 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
-const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -13,4 +9,4 @@ exports.config = {
   capabilities: {
   };
    'browserName': 'chrome'
- };
+ }
