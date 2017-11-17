23b678e9eaab912d0cbd95a9bb52837593639e4e
Houziaux mike <stahlilistenvoid@gmail.com>
2017-11-17 10:20:42 +0100

    craquage

--- protractor.conf.js
+++ protractor.conf.js
@@ -20,10 +20,4 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
 };
