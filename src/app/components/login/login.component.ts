900edb64bcb7235a4cdde0ab312c002b0df1c64f
tiboCorb <corbrionthibault@gmail.com>
2018-02-09 14:12:18 +0100

    upgrade angular 4 to 5.2 and add  material css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Response, Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import {Headers} from '@angular/http';
+import {Headers , RequestOptions} from '@angular/http';
 import {Router} from '@angular/router'
 
 @Component({
@@ -23,9 +23,12 @@ export class LoginComponent implements OnInit {
       formData.append('_password', this.password)
 
       let headers = new Headers();
+      let requestOptions= new RequestOptions();
       headers.append('Content-Type', 'application/x-www-form-urlencoded');
+      requestOptions.headers = headers; 
+      
       this.http
-          .post('http://localhost:8000/api/login_check', formData, headers)
+          .post('http://localhost:8000/api/login_check', formData, requestOptions)
           .subscribe(response => {
               if(response.status === 200){
                   var token = response.json().token;
