7c742a3b8aa2b12abfe41c738156203b77b41a0c
Houziaux mike <stahlilistenvoid@gmail.com>
2018-04-22 19:09:43 +0200

    update login for sf4 api

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import 'rxjs/add/operator/map';
-import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
 import {Router} from '@angular/router';
 import {environment} from '../../../environments/environment';
 
@@ -15,26 +15,21 @@ export class LoginComponent implements OnInit {
   private username:string;
   private password:string;
   public hide = true;
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: Http, private router: Router) { }
 
   ngOnInit() {
   }
 
-  submit() {
+    submit() {
+        this.http.post(`${environment.API}/login_check`, { username : this.username, password: this.password })
 
-      let formData: FormData = new FormData();
-      formData.append('_username', this.username);
-      formData.append('_password', this.password);
-
-      this.http.post(`${environment.API}/login_check`, formData)
-
-          .subscribe(response => {
-              if (response['token']!=="") {
-                  var token = response['token'];
-                  localStorage.setItem('token', token);
-                  this.router.navigateByUrl('/home/dashboard');
-              } 
-          });
-  }
+            .subscribe(response => {
+                if (response['token']!=="") {
+                    var token = response['token'];
+                    localStorage.setItem('token', token);
+                    this.router.navigateByUrl('/home/dashboard');
+                }
+            });
+    }
 
 }
