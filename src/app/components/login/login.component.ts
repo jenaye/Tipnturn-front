02d1897a71cebc3b0806165bf6e9612867bf54c5
Houziaux mike <stahlilistenvoid@gmail.com>
2018-05-25 10:15:00 +0200

    hotfix pr in API isnt merged

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import 'rxjs/add/operator/map';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import {Router} from '@angular/router';
 import {environment} from '../../../environments/environment';
 
@@ -15,21 +15,26 @@ export class LoginComponent implements OnInit {
   private username:string;
   private password:string;
   public hide = true;
-  constructor(private http: Http, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
-    submit() {
-        this.http.post(`${environment.API}/login_check`, { username : this.username, password: this.password })
+  submit() {
 
-            .subscribe(response => {
-                if (response['token']!=="") {
-                    var token = response['token'];
-                    localStorage.setItem('token', token);
-                    this.router.navigateByUrl('/home/dashboard');
-                }
-            });
-    }
+      let formData: FormData = new FormData();
+      formData.append('_username', this.username);
+      formData.append('_password', this.password);
+
+      this.http.post(`${environment.API}/login_check`, formData)
+
+          .subscribe(response => {
+              if (response['token']!=="") {
+                  var token = response['token'];
+                  localStorage.setItem('token', token);
+                  this.router.navigateByUrl('/home/dashboard');
+              } 
+          });
+  }
 
 }
