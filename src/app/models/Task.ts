33e1b11b5aa54fd0ded70422e5d821e88574d97a
tiboCorb <corbrionthibault@gmail.com>
2018-07-08 19:49:35 +0200

    User model add

--- src/app/models/Task.ts
+++ src/app/models/Task.ts
@@ -1,22 +1,23 @@
 import { Tag } from '../models/Tag';
+import { User } from '../models/User'
 
 export class Task {
     public tags: Array<Tag>;
     public title: string;
     public deadline: string;
     public comment: string;
-    public user: string;
+    public user: User;
     public state: string;
     public id: number;
 
-    constructor(taskTitle : string , taskState : string,taskId?:number, taskTag? : Array<Tag> , taskDeadLine?:string, taskComment?:string,taskUser?:string){
+    constructor(taskTitle : string , taskState : string,taskId?:number, taskTag? : Array<Tag> , taskDeadLine?:string, taskComment?:string,taskUser?:User){
         this.title = taskTitle;
         this.state= taskState;
         if(taskId){this.id=taskId}
         if(taskTag){this.tags = taskTag}else{this.tags = new Array()}
         if(taskDeadLine){this.deadline=taskDeadLine}
         if(taskComment){ this.comment= taskComment}
-        if(taskUser && taskUser.trim()!==''){this.user= taskUser}
+        if(taskUser){this.user= taskUser}
     }
 
  
